0284885eed5069ce4cd01ad6d5e7a3af61e968a4
LEOSEBAS <leonardosebastian990@gmail.com>
2020-04-30 21:39:29 -0500

    Se han creado las 4 opciones del "Mas", una de regiones, otra de contactos, otra de más info y una de juego que está en evaluación

---
+++ lib/Contenidos/Mas/Contactos.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class Contactos extends StatelessWidget{
+
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Contactos"),
+      ),
+    );
+
+  }
+}
---
+++ lib/Contenidos/Mas/Juego.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class Juego extends StatelessWidget{
+
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Juego"),
+      ),
+    );
+
+  }
+}
